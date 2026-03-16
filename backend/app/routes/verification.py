88ee26e1ecfffab666b7291e6071489c8f3c01c3
BinaM122 <binambodji07@gmail.com>
2026-03-16 00:47:27 -0400

    added Firebase token verification decorator

---
+++ backend/app/routes/verification.py
@@ -0,0 +1,30 @@
+from flask import jsonify, request
+import logging
+from firebase_admin import auth
+from functools import wraps
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
+            return jsonify({'error': 'missing header'}), 401
+        
+        token = auth_header.split(' ')[1]
+        uid = verify_token_and_get_uid(token)
+
+        if not uid:
+            return jsonify({'error':'invalid UID'}), 401
+        return f(uid, *args, **kwargs)
+    return decorated
+
+def verify_token_and_get_uid(id_token):
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']
+        logging.info(f"Successfully verified token for user: {uid}")
+        return uid
+    except auth.FirebaseAuthError as e:
+        logging.error(f"Error verifying Firebase ID token: {e}")
+        return None
