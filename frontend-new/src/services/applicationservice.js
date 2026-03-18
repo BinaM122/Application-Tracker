ee4aeba9eb969d3fe1ff1a7805e26630e1940d80
BinaM122 <binambodji07@gmail.com>
2026-03-18 02:49:32 -0400

    added application service layer

--- frontend-new/src/services/applicationservice.js
+++ frontend-new/src/services/applicationservice.js
@@ -1,7 +1,47 @@
-import {getAuth, onAuthStateChanged, getIDToken}
+export const getApplications = async (token) => {
+    const response = await fetch('http://localhost:5001/api/applications/' , {
+    headers: {
+        'Authorization': `Bearer ${token}`
+    }
+        })
+const data = await response.json()
+return data
+}
 
+export const createApplication = async (token, data) => {
+    const create = await fetch('http://localhost:5001/api/applications/' , {
+        method : 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
 
+    const created = await create.json()
+    return created
+    }
 
-const getApplications = async (token) => {
+export const updateApplication = async (token, id, data) => {
+    const edit = await fetch(`http://localhost:5001/api/applications/${id}` , {
+        method : 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    const updated = await edit.json()
+    return updated
+}
 
+export const deleteApplication = async (token, id) => {
+    const remove = await fetch(`http://localhost:5001/api/applications/${id}` , {
+        method : 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    }) 
+    return remove
 }
